93e20a430678a601c622d8e8627c362b7c841a2e
Криворогов Данил <daniil.k@mail.ru>
2022-04-09 15:37:27 +0500

    доступ к дашборду без логина

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', function () {
+    return view('main');
+})->name('main');
 
 require __DIR__.'/auth.php';

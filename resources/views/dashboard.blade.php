0d89936a689fd8f8447319dc6eb6403dea0c66dc
Криворогов Данил <daniil.k@mail.ru>
2022-04-10 00:19:25 +0500

    галерея

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Магазин
         </h2>
     </x-slot>
 
@@ -19,11 +19,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-            @foreach ($items as $item)
+                <div class="grid grid-cols-2 md:grid-cols-3 gap-20 p-10">
 
-            
+                    @foreach ($items as $item)
 
-            @endforeach
+                        <div class="rounded shadow-lg">
+                            <img class="w-full rounded object-center" src="/external-content.jpeg">
+                            <div class="text-center">
+                                <div class="font-bold text-xl">{{$item->name}}</div>
+                                <div class="text-sm">100 баллов</div>
+                            </div>
+                        </div>
+
+                    @endforeach
+
+                </div>
 
             </div>
         </div>
@@ -31,7 +41,7 @@
 
     @else
 
-    Товаров по вашему запросу нет
+    <h1 class="text-center mt-5 text-lg">Товаров по вашему запросу нет</h1>
 
     @endif
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,12 +13,30 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        <span class="text-sm">Главная</span>
                     </x-nav-link>
                 </div>
+
+                <div class="flex items-center sm:ml-8">
+                    <div x-data="{dropdownMenuCategories: false}" class="relative">
+                        <!-- Dropdown toggle button -->
+                        <button @click="dropdownMenuCategories = ! dropdownMenuCategories">
+                            <span class="text-sm">Категории</span>
+                        </button>
+                        <!-- Dropdown list -->
+                        <div x-show="dropdownMenuCategories" class="absolute right-0 py-2 mt-2 bg-white bg-gray-100 rounded-md shadow-xl w-44">
+                            @foreach (App\Models\Category::All() as $item)
+                            <a href="{{ route('dashboard') }}/{{$item->name}}" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                {{$item->name}}
+                            </a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
-            <!-- Settings Dropdown -->
+            <!-- Log in/out Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 @if (Route::has('login'))
                         @auth

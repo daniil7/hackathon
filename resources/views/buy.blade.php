10754d0c3b9cf35f94ff4fa6478204c2fee49500
Albert <albert.fz@yandex.ru>
2022-04-10 09:14:37 +0500

    improve buy template

--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -8,6 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-3">
+                <img src="/images/{{App\Models\Product::find($id)->image}}">
                 <form class="needs-validation" method="POST" action="/product/buy">
                     @csrf
                     <div class="grid grid-cols-2 gap-1 mr-auto w-3/4 mx-2">
@@ -29,6 +30,7 @@
                     </div>
                     <input type="hidden" name="product_id" value="{{$id}}">
                     <button class="btn btn-primary m-2" type="submit"> Принять </button>
+                    <button class="btn btn-primary m-2" type="button"> Подписаться </button>
                 </form>
             </div>
         </div>
